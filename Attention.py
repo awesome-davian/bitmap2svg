fix softmax bug

Attention.py
@@ -33,7 +33,8 @@ class Attn(nn.Module):
 
 		de_hidden = de_hidden.unsqueeze(2)         #batch, feature_size, 1 
 		attn_weight = torch.bmm(en_out, de_hidden)   #batch, depth, 1 
-		attn_weight = F.softmax(attn_weight)
+		attn_weight = attn_weight.squeeze(2)
+		attn_weight = F.softmax(attn_weight).unsqueeze(2)
 		context = torch.bmm(attn_weight.transpose(2,1),en_out) #batch, 1, feature_size
 
 		return context

attn_model.py
@@ -151,14 +151,13 @@ class AttnDecoderRnn(nn.Module):
     def sample(self, features):
         """Samples captions for given image features (Greedy search)."""
         sampled_ids = []
-        inputs = features.unsqueeze(1)
         h,c = self.init_lstm(features)
 
-        for i in range(30):                                      # maximum sampling length
+        for i in range(10):                                      # maximum sampling length
             if i == 0:
                 #word_init = Variable(torch.LongTensor([1])).cuda()
                 #x = self.embed(word_init).unsqueeze(1)
-                x = Variable(torch.zeros(1,1,256)).cuda()
+                x = Variable(torch.rand(1,1,256)).cuda()
                 #sampled_ids.append(word_init)
             else: 
                 x = self.embed((predicted))
@@ -167,9 +166,15 @@ class AttnDecoderRnn(nn.Module):
             lstm_input = torch.cat((context, x) ,2)
             lstm_out, (h,c) = self.lstm(lstm_input, (h,c))          # (batch_size, 1, hidden_size), 
             out = self.decode_lstm(x, context, h, lstm_out)
-            #print(out)
+            # hidden = lstm_out.squeeze(0)
+            # out = self.h2out(hidden)
+            # context = context.squeeze(1)
+            # out += self.ctx2out(context)
+            # out += x
+            # out = F.tanh(out)
+            # out = self.out(out)
             predicted = out.max(1)[1]
-            print(predicted)
+            #print(predicted)
             sampled_ids.append(predicted)
         #print(sampled_ids)
         #sampled_ids = torch.cat(sampled_ids, 1)                  # (batch_size, 20)

train_attn.py
@@ -10,6 +10,7 @@ from torch.autograd import Variable
 from torch.nn.utils.rnn import pack_padded_sequence
 from torchvision import transforms
 import pickle
+import torch.nn.functional as F 
 
 def to_var(x, volatile=False):
     if torch.cuda.is_available():
@@ -99,6 +100,7 @@ def main(args):
                         loss.data[0], np.exp(loss.data[0]))) 
 
                 #test set accuracy 
+                #print(outputs.view(args.batch_size,-1))
                 print(outputs.max(1)[1].view(args.batch_size, -1))
                 outputs_np = outputs.max(1)[1].cpu().data.numpy()
                 targets_np = captions.cpu().data.numpy()
@@ -135,17 +137,17 @@ def main(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model_path', type=str, default='./models/attn/1object/' ,
+    parser.add_argument('--model_path', type=str, default='./models/attn/2object/' ,
                         help='path for saving trained models')
     parser.add_argument('--crop_size', type=int, default=128,
                         help='size for randomly cropping images')
-    parser.add_argument('--root_path', type=str, default='data/circle_and_rect_1k/',
+    parser.add_argument('--root_path', type=str, default='data/2object/',
                         help='path for root')
     parser.add_argument('--log_step', type=int , default=10,
                         help='step size for prining log info')
     parser.add_argument('--save_step', type=int , default=50,
                         help='step size for saving trained models')
-    parser.add_argument('--vocab_path', type=str, default='./data/attn/vocab1.pkl', 
+    parser.add_argument('--vocab_path', type=str, default='./data/attn/vocab2.pkl', 
                         help='path for saving vocabulary wrapper')
     # Model parameters
     parser.add_argument('--embed_size', type=int , default=256 ,